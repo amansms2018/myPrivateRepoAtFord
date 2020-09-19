86dadc9ee9b93d3c3972b21621dc86e45c7227b8
ATEKELAR <atekelar@ford.com>
2020-09-18 23:02:24 -0400

    new commmit

---
+++ Angular/UIComponentDemo/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { IconsComponent } from './nguicomponent-modules/icons/icons.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'icons', component: IconsComponent } ,
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular/UIComponentDemo/src/app/nguicomponent-modules/nguicomponent-modules.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsComponent } from './icons/icons.component';
+
+
+
+@NgModule({
+  declarations: [IconsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NGUIComponentModulesModule { }
